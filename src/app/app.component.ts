183c500c796fbd0c8260c1b17f1ba69ededebedd
MostafaZayed <mostafa.zayed@hypercell.net>
2025-03-18 16:58:01 +0200

    fix tailwind bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ExpreinceComponent } from "./expreince/expreince.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, ExpreinceComponent],
+  imports: [ HeaderComponent, MainComponent, ExpreinceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
